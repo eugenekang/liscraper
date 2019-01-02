dce94db0432849f4124ac4be5e968526c917e6b9
eugenekang <eugene.yc.kang@gmail.com>
2019-01-01 20:28:02 -0500

    Params file

---
+++ parameters.py
@@ -0,0 +1,10 @@
+# search query 
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
+
+
+# file were scraped profile information will be stored  
+file_name = 'results_file.csv'
+
+# login credentials
+linkedin_username = 'email@gmail.com'
+linkedin_password = 'xxxxxx'
